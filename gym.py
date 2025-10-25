5c5d20ccbc0c304d40ce75b29b415b83c8c521a4
absrdentity <cguyyy76@gmail.com>
2025-10-25 15:22:14 +0700

    Updated homepage and membership templates

--- gym.py
+++ gym.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify, request, url_for, redirect, render_template
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 app = Flask(__name__)
+app.secret_key = '!@#$'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'ftl_gym'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -13,6 +23,10 @@ def join_membership():
 @app.route('/membership', methods=['GET'])
 def membership():
     return render_template('membership.html')
+
+@app.route('/our-clubs')
+def our_clubs():
+    return render_template('our_clubs.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
