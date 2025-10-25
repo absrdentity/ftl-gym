d1353e72a7af11e9f76e7846acd57c13e408c62d
absrdentity <cguyyy76@gmail.com>
2025-10-25 23:56:33 +0700

    ganti nama dll

--- gym.py
+++ gym.py
@@ -8,7 +8,7 @@ app.secret_key = '!@#$'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'ftl_gym'
+app.config['MYSQL_DB'] = 'ftl-gym'
 
 mysql = MySQL(app)
 
@@ -17,18 +17,35 @@ def home():
     return render_template('home.html')
 
 @app.route('/join', methods=['GET', 'POST'])
-def join_membership():
-    if request.method == 'POST' and 'name' in request.form and 'email' and 'password' and 'phone' in request.form:
+def join():
+
+    selected_package = request.args.get('package', '')
+
+    if request.method == 'POST' and all(k in request.form for k in ['name', 'email', 'password', 'phone', 'package']):
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
         phone = request.form['phone']
+        package = request.form['package']
+
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('INSERT INTO members VALUES (NULL, %s, %s, %s, %s)', (name, email, password, phone))
+        cursor.execute(
+            'INSERT INTO members (name, email, password, phone, package) VALUES (%s, %s, %s, %s, %s)',
+            (name, email, password, phone, selected_package)
+        )
         mysql.connection.commit()
-        return redirect(url_for('member_home'))
-    else:
-        return render_template('join.html')
+
+        return redirect(url_for('member_home', name=name, package=selected_package))
+    
+    return render_template('join.html', selected_package=selected_package)
+
+
+@app.route('/member_home')
+def member_home():
+    name = request.args.get('name')
+    package = request.args.get('package')
+    return render_template('member_home.html', name=name, package=package)
+
 
 @app.route('/membership', methods=['GET'])
 def membership():
